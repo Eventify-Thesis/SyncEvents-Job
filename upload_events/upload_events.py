fb8701fd97f51afd5040bbf3c0448e3ed0d3166d
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2025-04-21 14:20:22 +0700

    fix: change in running file

--- upload_events/upload_events.py
+++ upload_events/upload_events.py
@@ -41,8 +41,8 @@ last_sync_time = load_last_sync_time_qdrant()
 
 # PostgreSQL connection
 conn = psycopg2.connect(
-    host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT"),
+    host=os.getenv("POSTGRES_HOST", "localhost"),
+    port=os.getenv("POSTGRES_PORT", "5432"),
     user=os.getenv("POSTGRES_USER"),
     password=os.getenv("POSTGRES_PASSWORD"),
     dbname=os.getenv("POSTGRES_DB")
